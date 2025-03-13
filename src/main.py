2c8574f23d6e45182712f3a5b337632af5c9bcb1
bbantu <brianbaraza40@gmail.com.com>
2025-03-13 15:59:44 +0300

    call create tabs to start our program with the different tabs created

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from nicegui import ui
+from tab.create_tab import create_tabs
 
 if __name__ in {"__main__", "__mp_main__"}:
-    pass
+    create_tabs()
     ui.run(title="Heart Attack Mortality Dashboard")
