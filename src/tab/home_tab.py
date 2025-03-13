0d5779e9f6a4f3c40357fef596178e41a8fc5504
bbantu <brianbaraza40@gmail.com.com>
2025-03-13 14:54:06 +0300

    Added 'Why It Matters' section to the Home tab

--- src/tab/home_tab.py
+++ src/tab/home_tab.py
@@ -10,3 +10,10 @@ def create_home_tab(tabs):
         with ui.card().classes('w-full max-w-3xl'):
             ui.label("Goal").classes('text-xl font-bold mb-2')
             ui.label("Understand and predict Heart Attack mortality patterns to improve patient outcomes").classes('text-lg')
+
+        # Why Section
+        with ui.card().classes('w-full max-w-3xl'):
+            ui.label("Why It Matters").classes('text-xl font-bold mb-2')
+            ui.label("""Accurately triaging patients and reducing time to treatment can save lives. 
+                    Early identification of high-risk patients enables faster intervention and better outcomes."""
+            ).classes('text-lg')
